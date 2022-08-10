91affad786c32e191afbb7f2267a69c2ae82443d
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-10 14:01:05 +0900

    add: Login Backend

--- src/users/users.mutations.js
+++ src/users/users.mutations.js
@@ -1,5 +1,6 @@
 import bycrypt from "bcrypt";
 import prisma from "../../client";
+import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
@@ -37,6 +38,31 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find user with args.username
+      const user = await prisma.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "user not found.",
+        };
+      }
+      // check password with args.password
+      const passwordOk = await bycrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+
+      // issue a token and send it to user
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
