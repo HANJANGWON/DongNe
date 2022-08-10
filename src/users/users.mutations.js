2951532e671e34135f7663701ee774f0150d5472
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-10 10:45:58 +0900

    add: Create Account Backend

---
+++ src/users/users.mutations.js
@@ -0,0 +1,42 @@
+import bycrypt from "bcrypt";
+import prisma from "../../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        const existingUser = await prisma.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username or password is already taken.");
+        }
+
+        const uglyPassword = await bycrypt.hash(password, 10);
+        return prisma.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+};
