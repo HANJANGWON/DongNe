2393be7af9d15c84bccc08d906d2543e1ec2c988
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-12 11:04:06 +0900

    add : protectedResolver Backend

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 import { ApolloServer, gql } from "apollo-server";
 import schema from "./schema";
+import { getUser, protectResolver } from "./src/users/users.utils";
 
 const server = new ApolloServer({
   schema,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      protectResolver,
+    };
+  },
 });
 const PORT = process.env.PORT;
 
--- src/users/editProfile/editProfile.resolvers.js
+++ src/users/editProfile/editProfile.resolvers.js
@@ -1,38 +1,43 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../../../client";
+import { protectResolver } from "../users.utils";
+
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  { loggedInUser, protectResolver }
+) => {
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "could not update profile.",
+    };
+  }
+};
 
 export default {
   Mutation: {
-    editProfile: async (
-      _,
-      { firstName, lastName, username, email, password: newPassword }
-    ) => {
-      let uglyPassword = null;
-      if (newPassword) {
-        uglyPassword = await bcrypt.hash(newPassword, 10);
-      }
-      const updatedUser = await prisma.user.update({
-        where: {
-          id: 1,
-        },
-        data: {
-          firstName,
-          lastName,
-          username,
-          email,
-          ...(uglyPassword && { password: uglyPassword }),
-        },
-      });
-      if (updatedUser.id) {
-        return {
-          ok: true,
-        };
-      } else {
-        return {
-          ok: false,
-          error: "could not update profile.",
-        };
-      }
-    },
+    editProfile: protectResolver(resolverFn),
   },
 };
