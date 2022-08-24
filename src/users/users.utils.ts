b334d8326fee877f63c91e6436bf6d3635bdf30f
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-24 13:28:27 +0900

    refactoring: protectResolver

--- src/users/users.utils.ts
+++ src/users/users.utils.ts
@@ -27,10 +27,15 @@ export const getUser = async (token) => {
 export function protectResolver(ourResolver: Resolver) {
   return function (root, args, context, info) {
     if (!context.loggedInUser) {
-      return {
-        ok: false,
-        error: "Please log in to perform this action.",
-      };
+      const query = info.operation.operation === "query";
+      if (query) {
+        return null;
+      } else {
+        return {
+          ok: false,
+          error: "Please log in to perform this action.",
+        };
+      }
     }
     return ourResolver(root, args, context, info);
   };
