ada8dfa435eaf2685a9507eb29ca986d3c48ee0e
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-25 23:45:48 +0900

    update : Room Exists Backend

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,10 +1,33 @@
+import { withFilter } from "graphql-subscriptions";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
+import prisma from "../../client";
 
-export default {
+const resolvers = {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (parent, args, context, info) => {
+        const room = await prisma.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!room) {
+          throw new Error("You shall not see this.");
+        }
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(parent, args, context, info);
+      },
     },
   },
 };
+
+export default resolvers;
