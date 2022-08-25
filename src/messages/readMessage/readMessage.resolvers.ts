bddabca712152aab95815645aaac70a740341509
HANJANGWON <wkddnjsdl00@naver.com>
2022-08-25 13:08:11 +0900

    add : Read Message

---
+++ src/messages/readMessage/readMessage.resolvers.ts
@@ -0,0 +1,48 @@
+import { Resolvers } from "src/types";
+import { protectResolver } from "src/users/users.utils";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    readMessage: protectResolver(
+      async (_, { id }, { loggedInUser, prisma }) => {
+        const message = await prisma.message.findFirst({
+          where: {
+            id,
+            userId: {
+              not: loggedInUser.id,
+            },
+            room: {
+              users: {
+                some: {
+                  id: loggedInUser.id,
+                },
+              },
+            },
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!message) {
+          return {
+            ok: false,
+            error: "Message not found",
+          };
+        }
+        await prisma.message.update({
+          where: {
+            id,
+          },
+          data: {
+            read: true,
+          },
+        });
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
+
+export default resolvers;
